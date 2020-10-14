cf3f734b471b55ce7bfabbcb008f3cbf16152377
Akanksha Pandey <akanksha.pandey@ul.com>
2020-10-15 01:09:07 +0530

    Updated code

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ function displayTemperature(response){
   let humidityElement=document.querySelector("#humidity");
   let speedElement=document.querySelector("#speed");
   let dateElement=document.querySelector("#date");
+  let iconElement=document.querySelector("#weather-icon");
  
   temperatureElement.innerHTML=Math.round(response.data.main.temp);
   cityElement.innerHTML=response.data.name;
@@ -30,6 +31,8 @@ function displayTemperature(response){
   humidityElement.innerHTML=response.data.main.humidity;
   speedElement.innerHTML=Math.round(response.data.wind.speed);
   dateElement.innerHTML=formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt",response.data.weather[0].description);
   console.log(response.data);
 }
 
