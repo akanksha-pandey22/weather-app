384c68660691e3f04330358c9c4edfc22f19a45b
Akanksha Pandey <akanksha.pandey@ul.com>
2020-10-15 00:50:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,10 +33,8 @@ function displayTemperature(response){
   console.log(response.data);
 }
 
+let city="New Delhi";
 let apikey="fef38b882b534b153098ddc0f2902952";
-let apiUrl=`https:api.openweathermap.org/data/2.5/weather?q=New Delhi&appid=${apikey}&units=metric`;
-
-
-
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
