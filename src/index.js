f92397e093ddbdd10e6e93e031cadeb1c7472e1c
Akanksha Pandey <akanksha.pandey@ul.com>
2020-10-15 19:26:23 +0530

    Updated forecast section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 function formatDate(timestamp){
   let date=new Date(timestamp);
+  
+  let days= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+  let day=days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`
+
+
+}
+
+function formatHours(timestamp){
+  let date=new Date(timestamp);
   let hours=date.getHours();
   if (hours<10){
     hours=`0${hours}`;
@@ -9,12 +19,9 @@ function formatDate(timestamp){
   if (minutes<10){
     minutes=`0${minutes}`;
   }
-  let days= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-  let day=days[date.getDay()];
-  return `${day} ${hours}:${minutes}`
-
-
+  return `${hours}:${minutes}`;
 }
+
 
 function displayTemperature(response){
   console.log(response.data.main.temp);
@@ -40,10 +47,44 @@ function displayTemperature(response){
 }
 let apiKey="fef38b882b534b153098ddc0f2902952";
 
+
+function displayForecast(response){
+  
+  let forecastElement = document.querySelector("#forecast-weather");
+  forecastElement.innerHTML=null;
+  let forecast=null;
+
+for (let index = 0; index < 6; index++) {
+  console.log(forecast);
+  forecast=response.data.list[index];
+  forecastElement.innerHTML+= `<div class="col2 mt-5">
+  <h5>
+  ${formatHours(forecast.dt * 1000)}
+  </h5>
+  <img 
+  src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" 
+  alt=""  />
+  <div class="forecast-temperature">
+  <strong>${Math.round(forecast.main.temp_max)}°</strong>
+  ${Math.round(forecast.main.temp_min)}°
+</div>
+  </div>`;
+   
+}
+
+}
+
+
 function search(city){
  
   let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+
+
+
 }
 
 
@@ -58,8 +99,8 @@ function handleSubmit(event){
 function displayFahrenheitTemperature(event){
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-celsiusLink.classList.remove("active");
-fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
 
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 +32;
   temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
@@ -70,12 +111,12 @@ function displayCelsiusTemperature(event){
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   celsiusLink.classList.add("active");
-fahrenheitLink.classList.remove("active");
+  fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML=Math.round(celsiusTemperature);
 
 }
 
-search("New Delhi");
+search("Mumbai");
 
 function showPosition(position) {
   let lon = position.coords.longitude;
@@ -83,7 +124,10 @@ function showPosition(position) {
   let unit = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&units=${unit}`;
   console.log(apiUrl);
+  
   axios.get(apiUrl).then(displayTemperature);
+
+  
 }
 
 function getLocation() {
